d26c3adfecc5a58f02a30e10a43bc968a5160a63
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-08 18:34:15 -0300

    Atualiza o modelo produto utilizando lombok

--- src/main/java/com/minhalista/appMinhaLista/model/domain/Produto.java
+++ src/main/java/com/minhalista/appMinhaLista/model/domain/Produto.java
@@ -1,10 +1,16 @@
 package com.minhalista.appMinhaLista.model.domain;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Produto {
 
     private  Integer id;
@@ -13,62 +19,6 @@ public class Produto {
     private String descricao;
     private Double preco;
     private LocalDateTime dataCadastro;
-
-    public Produto(Integer id, String nome, String imagem, String descricao, Double preco, LocalDateTime dataCadastro) {
-        this.id = id;
-        this.nome = nome;
-        this.imagem = imagem;
-        this.descricao = descricao;
-        this.preco = preco;
-        this.dataCadastro = dataCadastro;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getImagem() {
-        return imagem;
-    }
-
-    public void setImagem(String imagem) {
-        this.imagem = imagem;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-
-    public LocalDateTime getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public void setDataCadastro(LocalDateTime dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
 
     @Override
     public String toString() {
