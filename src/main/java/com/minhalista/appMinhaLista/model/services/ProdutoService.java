30f00ed637f4865e4211ebccb7a89caab93814dd
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-09 11:33:33 -0300

    Atualiza a classe ProdutoService para trabalhar com repository

---
+++ src/main/java/com/minhalista/appMinhaLista/model/services/ProdutoService.java
@@ -0,0 +1,26 @@
+package com.minhalista.appMinhaLista.model.services;
+
+import com.minhalista.appMinhaLista.model.domain.Produto;
+import com.minhalista.appMinhaLista.model.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    public void incluir(Produto produto) {
+        produtoRepository.save(produto);
+    }
+    public Collection<Produto> listar() {
+        return (Collection<Produto>) produtoRepository.findAll();
+    }
+    public Optional<Produto> buscar(Integer id) {
+        return produtoRepository.findById(id);
+    }
+}
