30db3c2a782ddd9d03eb4f2e1d4c285956032c04
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-10 18:07:59 -0300

    Atualiza modelo produto com o mapeamento para a lista

--- src/main/java/com/minhalista/appMinhaLista/model/domain/Produto.java
+++ src/main/java/com/minhalista/appMinhaLista/model/domain/Produto.java
@@ -1,15 +1,13 @@
 package com.minhalista.appMinhaLista.model.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Setter
 @Getter
@@ -17,21 +15,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 
 @Entity
+@Table(name = "produtos")
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Integer id;
-
+    private Integer id;
     private String nome;
     private String imagem;
     private String descricao;
     private Double preco;
+    @ManyToMany(mappedBy = "produtos")
+    private List<Lista> listas;
     private LocalDateTime dataCadastro;
 
-    @Override
-    public String toString() {
-        return String.format("nome (%s) - preco (%.2f) - descricao (%s) - dataCadastro (%s)",
-                nome, preco, descricao, dataCadastro);
-    }
+//    @Override
+//    public String toString() {
+//        return String.format("nome (%s) - preco (%.2f) - descricao (%s) - dataCadastro (%s)",
+//                nome, preco, descricao, dataCadastro);
+//    }
 }
