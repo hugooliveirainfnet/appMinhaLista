a33a094cbd1268ccb61d583682d3bb29b3cf4ba4
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-10 18:16:16 -0300

    Cria a classe de apoio quando se relaciona usuarios e grupos

---
+++ src/main/java/com/minhalista/appMinhaLista/model/domain/Integrante.java
@@ -0,0 +1,22 @@
+package com.minhalista.appMinhaLista.model.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "integrantes")
+public class Integrante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "usuarioId")
+    private Usuario usuario;
+    @ManyToOne
+    @JoinColumn(name = "grupoId")
+    private Grupo grupo;
+    private LocalDateTime dataAdicao;
+
+}
