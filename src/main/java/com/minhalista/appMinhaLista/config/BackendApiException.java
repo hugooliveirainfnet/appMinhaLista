afeb97dd158d10f4adffe522f6968de649d72b54
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-22 10:43:25 -0300

    Cria um controlador para trabalhar com as excessões geradas pela api

--- src/main/java/com/minhalista/appMinhaLista/config/BackendApiException.java
+++ src/main/java/com/minhalista/appMinhaLista/config/BackendApiException.java
@@ -1,6 +1,7 @@
 package com.minhalista.appMinhaLista.config;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +19,15 @@ public class BackendApiException extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleEmptyResultDataAccessException(NoSuchElementException ex, WebRequest request) {
         String bodyOfResponse = "Entidade não encontrada";
         return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+        String bodyOfResponse = "Campo informado é inválido ou inexistente";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    protected ResponseEntity<Object> handleInvalidDataAccessApiUsageException(InvalidDataAccessApiUsageException ex, WebRequest request) {
+        String bodyOfResponse = "Campo informado é inválido ou inexistente";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
