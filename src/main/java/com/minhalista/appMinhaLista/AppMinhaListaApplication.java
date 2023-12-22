b77089418883ca2dc3ab92f715952fe8ceb2d007
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-22 10:44:03 -0300

    Atualiza o projeto para usar a dependencia do feign

--- src/main/java/com/minhalista/appMinhaLista/AppMinhaListaApplication.java
+++ src/main/java/com/minhalista/appMinhaLista/AppMinhaListaApplication.java
@@ -3,11 +3,12 @@ package com.minhalista.appMinhaLista;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppMinhaListaApplication {
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@SpringBootApplication
+@EnableFeignClients
+public class AppMinhaListaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppMinhaListaApplication.class, args);
 	}
-
 }
